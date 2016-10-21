6c61c4f0739595e51297fe45b59c17db9af2ec5e
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2016-10-21 18:47:31 +0200

    Parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate clap;
+
 use std::io;
 use std::fs;
 use std::path::Path;
@@ -14,10 +17,19 @@ fn list_dir(dir: &Path) -> io::Result<()> {
     Ok(())
 }
 
-    
-
 fn main() {
-    match list_dir(Path::new(".")) {
+    let argv_matches = clap::App::new("etree")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("Extended tree")
+        .arg(clap::Arg::with_name("DIR")
+            .help("The directory to list")
+            .index(1))
+        .get_matches();
+
+    let dir = Path::new(argv_matches.value_of("DIR").unwrap_or("."));
+ 
+    match list_dir(dir) {
         Ok(_) => (),
         Err(err) => println!("{}", err),
     }
