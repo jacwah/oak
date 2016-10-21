0347274fb06cee42be198757e779dadf8d83eb91
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2016-10-21 17:38:04 +0200

    Implement simple ls

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+use std::fs;
+use std::path::Path;
+
+fn list_dir(dir: &Path) -> io::Result<()> {
+    for entry in try!(fs::read_dir(dir)) {
+        let entry = try!(entry);
+        let path = entry.path();
+        match path.file_name() {
+            Some(name) => println!("{}", name.to_string_lossy()),
+            None => (),
+        }
+    }
+    Ok(())
+}
+
+    
+
 fn main() {
-    println!("Hello, world!");
+    match list_dir(Path::new(".")) {
+        Ok(_) => (),
+        Err(err) => println!("{}", err),
+    }
 }
