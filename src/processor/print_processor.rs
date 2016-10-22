d59341b9a7280bd83cd80fae7e2f45a922b38250
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2016-10-22 06:56:45 +0200

    Print summary

--- src/processor/print_processor.rs
+++ src/processor/print_processor.rs
@@ -15,12 +15,18 @@ impl Dir {
 
 pub struct PrintProcessor {
     dir_stack: Vec<Dir>,
+    num_dirs: usize,
+    num_files: usize,
 }
 
 impl PrintProcessor {
 
     pub fn new() -> PrintProcessor {
-        PrintProcessor { dir_stack: Vec::new() }
+        PrintProcessor {
+            dir_stack: Vec::new(),
+            num_dirs: 0,
+            num_files: 0,
+        }
     }
 
     fn print_entry(&mut self, name: &str) {
@@ -50,22 +56,37 @@ impl PrintProcessor {
         println!("{}", name);
     }
 
+    fn print_summary(&self) {
+        // Do not count the root dir or underflow
+        let dirs = match self.num_dirs {
+            0 => 0,
+            n @ _ => n - 1,
+        };
+
+        println!("\n{} directories, {} files", dirs, self.num_files);
+    }
+
 }
 
 impl TreeProcessor for PrintProcessor {
     
     fn open_dir(&mut self, name: &str, num_entries: usize) {
         self.print_entry(name);
-        // incr processed
         self.dir_stack.push(Dir::new(num_entries));
+        self.num_dirs += 1;
     }
 
     fn close_dir(&mut self) {
         self.dir_stack.pop();
+
+        if self.dir_stack.is_empty() {
+            self.print_summary();
+        }
     }
 
     fn file(&mut self, name: &str) {
         self.print_entry(name);
+        self.num_files += 1;
     }
 
 }
