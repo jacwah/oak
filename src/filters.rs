1618a69a34927265f959a63ec1e14cef28de15c5
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2016-10-30 01:34:36 +0200

    Add gitignore filter

--- src/filters.rs
+++ src/filters.rs
@@ -1,5 +1,30 @@
+extern crate git2;
+
 use std::path::Path;
 use std::os::unix::ffi::OsStrExt;
+use self::git2::Repository;
+
+pub struct GitignoreFilter {
+    repo: Repository,
+}
+
+impl GitignoreFilter {
+    pub fn new(path: &Path) -> Result<Self, git2::Error> {
+        match Repository::discover(path) {
+            Ok(repo) => Ok(GitignoreFilter { repo: repo }),
+            Err(err) => Err(err),
+        }
+    }
+
+    pub fn filter(&self, path: &Path) -> bool {
+        // ./filename paths doesn't seem to work with should_ignore
+        let path = path.canonicalize().unwrap();
+        match self.repo.status_should_ignore(&path) {
+            Ok(result) => !result,
+            Err(_) => false,
+        }
+    }
+}
 
 pub fn filter_hidden_files(path: &Path) -> bool {
     // Is this implementation sound?
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ extern crate clap;
 extern crate ntree;
 
 use std::path::Path;
+use std::process;
 use ntree::print_processor::{PrintProcessor, SummaryFormat};
 use ntree::tree;
-use ntree::filters::{filter_hidden_files, filter_non_dirs};
+use ntree::filters::{filter_hidden_files, filter_non_dirs, GitignoreFilter};
 
 fn main() {
     let argv_matches = clap::App::new("ntree")
@@ -21,12 +22,19 @@ fn main() {
         .arg(clap::Arg::with_name("d")
              .help("List directories only")
              .short("d"))
+        .arg(clap::Arg::with_name("git-ignore")
+             .help("Do not list git ignored files")
+             .short("g"))
         .get_matches();
 
     let dir = Path::new(argv_matches.value_of("DIR").unwrap_or("."));
 
     let filter_hidden_files_ref = &filter_hidden_files;
     let filter_non_dirs_ref = &filter_non_dirs;
+    let filter_gitignore_maybe = GitignoreFilter::new(dir);
+    let filter_gitignore: GitignoreFilter;
+    let filter_gitignore_clos;
+    let filter_gitignore_ref;
 
     let mut filters: Vec<&Fn(&Path) -> bool> = Vec::new();
 
@@ -43,10 +51,28 @@ fn main() {
         procor.set_summary_format(SummaryFormat::DirCount);
     }
 
+    if argv_matches.is_present("git-ignore") {
+        match filter_gitignore_maybe {
+            Ok(_) => {
+                filter_gitignore = filter_gitignore_maybe.unwrap();
+                filter_gitignore_clos = |p: &Path| filter_gitignore.filter(p);
+                filter_gitignore_ref = &filter_gitignore_clos;
+                filters.push(filter_gitignore_ref);
+            },
+            Err(err) => {
+                println!("{}", err);
+                process::exit(1);
+            },
+        }
+    }
+
     match tree::process(&dir,
                         &mut procor,
                         &filters) {
         Ok(_) => (),
-        Err(err) => println!("error: {}", err),
+        Err(err) => {
+            println!("error: {}", err);
+            process::exit(1);
+        }
     }
 }
