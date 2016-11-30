40724bf55dce7530de55c1f829c44fdf89c61e1d
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2016-11-30 21:21:20 +0100

    Remove unused tree code

--- src/main.rs
+++ src/main.rs
@@ -62,7 +62,7 @@ fn main() {
     // if let Err(err) = tree::process(&dir, &mut procor, &filters) {
     //     die(&err);
     // }
-    if let Err(err) = tree::process2(dir, filters) {
+    if let Err(err) = tree::process(dir, filters) {
         die(&err);
     }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -2,7 +2,6 @@ use std::fs;
 use std::path::{Path, PathBuf};
 use std::error::Error;
 use std::iter::Peekable;
-use super::tree_processor::TreeProcessor;
 use super::filters::FileFilter;
 
 #[derive(Debug)]
@@ -100,43 +99,9 @@ impl Iterator for TreeIter {
     }
 }
 
-pub fn process2<F: FileFilter + 'static>(dir: &Path, file_filter: F) -> Result<(), Box<Error>> {
+pub fn process<F: FileFilter + 'static>(dir: &Path, file_filter: F) -> Result<(), Box<Error>> {
     for entry in try!(TreeIter::new(dir, file_filter)) {
         println!("{:?}", try!(entry));
     }
-    Ok(())
-}
-
-pub fn process<T, F>(dir: &Path, procor: &mut T, filter: &F) -> Result<(), Box<Error>>
-    where T: TreeProcessor,
-          F: FileFilter,
-{
-    let read_entries = try!(fs::read_dir(dir));
-
-    let entries: Vec<_> = try!(read_entries.collect());
-    let filter_results: Vec<_> = try!(entries.iter()
-        .map(|entry| filter.filter(&entry.path()))
-        .collect());
-    let entries: Vec<_> = entries.into_iter()
-        .zip(filter_results.into_iter())
-        .filter(|t| t.1)
-        .map(|t| t.0)
-        .collect();
-    procor.open_dir(dir, entries.len());
-
-    for entry in entries {
-        let path = entry.path();
-
-        let file_type = try!(entry.file_type());
-
-        if file_type.is_dir() {
-            try!(process(&path, procor, filter));
-        } else {
-            procor.file(&path);
-        }
-    }
-
-    procor.close_dir();
-
     Ok(())
 }
